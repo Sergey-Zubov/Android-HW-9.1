1ef23b9151f80577e7139b5e79391f84b3322485
Sergey Zubov <serjio.zubov@gmail.com>
2020-07-11 21:18:46 +0300

    for check

--- app/src/main/java/com/szubov/android_hw_91/MainActivity.java
+++ app/src/main/java/com/szubov/android_hw_91/MainActivity.java
@@ -1,8 +1,8 @@
 package com.szubov.android_hw_91;
 
+import androidx.annotation.StyleRes;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,26 +14,28 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static @StyleRes int themRes = R.style.AppTheme;
+    private @StyleRes int choiceTheme;
     private Spinner mSpinnerLoc;
     private Spinner mSpinnerTheme;
     private Button mBtnOk;
     private TextView mTextView;
     private Locale choiceLoc;
-    private Resources.Theme choiceTheme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(themRes);
         setContentView(R.layout.activity_main);
 
         initViews();
-
     }
 
     private void initViews() {
         mBtnOk = findViewById(R.id.btnOk);
         mTextView = findViewById(R.id.textView);
         mSpinnerLoc = findViewById(R.id.spinnerLoc);
+        mSpinnerTheme = findViewById(R.id.spinnerThemeColor);
 
         initSpinnerLoc();
         initSpinnerTheme();
@@ -44,46 +46,14 @@ public class MainActivity extends AppCompatActivity {
                 if (!Locale.getDefault().equals(choiceLoc)) {
                     changeLoc(choiceLoc);
                 }
-            }
-        });
 
-    }
-
-    private void initSpinnerTheme() {
-        ArrayAdapter<CharSequence> adapterTheme = ArrayAdapter.createFromResource
-                (this,R.array.spinner_color_theme, android.R.layout.simple_spinner_item);
-        adapterTheme.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mSpinnerTheme.setAdapter(adapterTheme);
-
-        mSpinnerTheme.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String[] theme = getResources().getStringArray(R.array.spinner_color_theme);
-                switch (theme[position]) {
-                    case "Стандартная":
-                    case "Standard":
-                        choiceTheme.;
-                        break;
-                    case "Черная":
-                    case "Black":
-
-                        break;
-                    case "Зеленая":
-                    case "Green":
-
-                        break;
-                    case "Синяя":
-                    case "Blue":
-
-                        break;
+                if (themRes != choiceTheme) {
+                    changeTheme(choiceTheme);
                 }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
 
             }
         });
+
     }
 
     private void initSpinnerLoc() {
@@ -95,11 +65,48 @@ public class MainActivity extends AppCompatActivity {
         mSpinnerLoc.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String[] language = getResources().getStringArray(R.array.spinner_loc_array);
-                if (language[position].equals("Английский") || language[position].equals("English")) {
+                String[] languages = getResources().getStringArray(R.array.spinner_loc_array);
+                if (languages[position].equals("Английский") || languages[position].equals("English")) {
                     choiceLoc = new Locale("en");
                 } else {
                     choiceLoc = new Locale("ru");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void initSpinnerTheme() {
+        ArrayAdapter<CharSequence> adapterTheme = ArrayAdapter.createFromResource
+                (this,R.array.spinner_color_theme_array, android.R.layout.simple_spinner_item);
+        adapterTheme.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinnerTheme.setAdapter(adapterTheme);
+
+        mSpinnerTheme.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String[] themes = getResources().getStringArray(R.array.spinner_color_theme_array);
+                switch (themes[position]) {
+                    case "Стандартная":
+                    case "Standard":
+                        choiceTheme = R.style.AppTheme;
+                        break;
+                    case "Черная":
+                    case "Black":
+                        choiceTheme = R.style.AppTheme_Black;
+                        break;
+                    case "Зеленая":
+                    case "Green":
+                        choiceTheme = R.style.AppTheme_Green;
+                        break;
+                    case "Синяя":
+                    case "Blue":
+                        choiceTheme = R.style.AppTheme_Blue;
+                        break;
                 }
             }
 
@@ -116,14 +123,11 @@ public class MainActivity extends AppCompatActivity {
         configuration.setLocale(choiceLoc);
         getResources().updateConfiguration(configuration,
                 getBaseContext().getResources().getDisplayMetrics());
-
         recreate();
     }
 
-    private void changeTheme() {
-        if (!this.getTheme().equals(choiceTheme))
-        getTheme().setTo(choiceTheme);
-
+    private void changeTheme(@StyleRes int choiceTheme) {
+        themRes = choiceTheme;
         recreate();
     }
 }
